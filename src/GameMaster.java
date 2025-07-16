78dcb19d0ade9867a9489ebb68e26b5ce7ac7978
HoriMasataka <241107@std.hi-joho.ac.jp>
2025-07-16 09:39:04 +0900

    2025/07/16

--- src/GameMaster.java
+++ src/GameMaster.java
@@ -18,7 +18,7 @@ public class GameMaster {
         monsters.add(goblin);
         monsters.add(slime);
 
-        System.out.println("---味方パーティー---");
+        System.out.println("---味方パーティ---");
         for(Character character : party) {
             character.showStatus();
         }
@@ -28,10 +28,44 @@ public class GameMaster {
             monster.showStatus();
         }
 
-        System.out.println("味方の総攻撃！");
+        System.out.println("\n味方の総攻撃！");
         for(Character character : party) {
             for(Monster monster : monsters) {
                 character.attack(monster);
+            }
+        }
+        System.out.println("\n敵の総攻撃！");
+        for(Monster monster : monsters) {
+            for(Character character : party) {
+                monster.attack(character);
+            }
+        }
+
+        System.out.println("\nダメージを受けた勇者が突然光だした！");
+        SuperHero superHero = new SuperHero(hero);
+        System.out.println("勇者はスーパーヒーローに進化した！");
+        for(Monster monster : monsters) {
+            superHero.attack(monster);
+        }
+
+        System.out.println("\n---味方パーティ最終ステータス---");
+        for(Character character : party) {
+            character.showStatus();
+            System.out.print("生存状況：");
+            if(character.isAlive()){
+                System.out.println("生存");
+            }else{
+                System.out.println("戦闘不能");
+            }
+        }
+        System.out.println("\n---敵グループ最終ステータス---");
+        for(Monster monster : monsters) {
+            monster.showStatus();
+            System.out.print("生存状況：");
+            if(monster.isAlive()){
+                System.out.println("生存");
+            }else{
+                System.out.println("討伐済み");
             }
         }
     }
